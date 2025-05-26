3b332250286dbdd8c7b3f2eef68217b3b758ebbb
chenveilleur <chenveilleur@gmail.com>
2025-05-26 13:14:23 +0800

    Update app.js

--- miniprogram-6/miniprogram/app.js
+++ miniprogram-6/miniprogram/app.js
@@ -1,8 +1,22 @@
-const request0 = require('./utils/request.js');
-
-App({
-    globalData: {
-      userId: request0.getUserId
-    }
-  })
-  
+    // app.js
+    App({
+        onLaunch() {
+        // 初始化云开发
+        wx.cloud.init({
+            env: 'cloud1-8groookrad2b1f6d', // 如：cloud1-xxxxx
+            traceUser: true
+        });
+    
+        // 调用云函数获取 openid
+        wx.cloud.callFunction({
+            name: 'getopenid'
+        }).then(res => {
+            const openid = res.result.openid;
+            console.log('获取到 openid：', openid);
+            wx.setStorageSync('userId', openid); // 存入本地缓存
+        }).catch(err => {
+            console.error('获取 openid 失败：', err);
+        });
+        }
+    });
+    
